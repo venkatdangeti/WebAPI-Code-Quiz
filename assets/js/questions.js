5460eb8bd966c2de318c1aeb14f96fe947f64923
Venkatdangeti <dangeti.venkat@gmail.com>
2024-01-14 15:02:44 +0000

    Added questions and Answers to question javascript file

---
+++ assets/js/questions.js
@@ -0,0 +1,92 @@
+
+// List of question and answers - source W3 school tutorials
+var questions = [
+  {
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: [
+      "<js>",
+      "<javascript>",
+      "<script>",
+      "<scripting>"
+    ],
+    answer: "<script>"
+  },
+  {
+    title: "Boolean operators that can be used in JavaScript include:",
+    choices: [
+      "'And' Operator &&",
+      "'Or' Operator ||",
+      "'Not' Operator !",
+      "All the above"
+    ],
+    answer: "All the above"
+  },
+  {
+    title: "What is the data type of variables in JavaScript?",
+    choices: [
+      "Object data types",
+      "Function data type",
+      "None of the above",
+      "All of the above"
+    ],
+    answer: "Object data types"
+  },
+  {
+    title: "How to write an IF statement in JavaScript?",
+    choices: [
+      "if i = 5 then",
+      "if i == 5 then",
+      "if (i == 5)",
+      "if i = 5"
+    ],
+    answer: "if (i == 5)"
+  },
+  {
+    title: "How do you round the number 7.25, to the nearest integer?",
+    choices: [
+      "round(7.25)",
+      "rnd(7.25)",
+      "Math.rnd(7.25)",
+      "Math.round(7.25)"
+    ],
+    answer: "Math.round(7.25)"
+  },
+
+  {
+    title: "Which event occurs when the user clicks on an HTML element?",
+    choices: [
+      "onchange",
+      "onmouseover",
+      "onclick",
+      "onmouseclick"
+    ],
+    answer: "onclick"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log"
+  },
+  {
+    title: "What is the type of Pop up boxes available in JavaScript?:",
+    choices: ["Alert", "Confirm", "Prompt", "All the above"],
+    answer: "All the above"
+  }
+]
